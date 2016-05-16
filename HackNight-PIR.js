797ae61cf1f7380200d63cae322f9a80d35f6988
Kyle Kurz <kyle@doublekaudio.com>
2016-05-16 05:45:54 +0000

    Actually run the GPIO, proper true/false comparisons

--- HackNight-PIR.js
+++ HackNight-PIR.js
@@ -9,7 +9,7 @@ const pin = argv["pin"]?argv["pin"]:7;
 //callback when the state changes
 gpio.on('change', function(channel, value) {
     console.log('Channel ' + channel + ' value is now ' + value);
-    if (value === 1) {
+    if (value === true) {
         io.sockets.emit('message', { message: 'The table is in use!!!' });
     } else {
         io.sockets.emit('message', { message: 'The table is free!!!' });
@@ -17,7 +17,7 @@ gpio.on('change', function(channel, value) {
 });
 
 //set up the gpio for interrupts on both rising and falling edge
-//gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 const app = express.createServer();
 app.use(express.static("public"));
