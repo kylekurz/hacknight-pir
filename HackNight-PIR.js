279c5b5cc879d85740ccde13e30f1f7f885ce4e0
Kyle Kurz <kyle@doublekaudio.com>
2016-05-17 03:44:09 +0000

    Emit state as soon as a client connects. Timestamps on logs

--- HackNight-PIR.js
+++ HackNight-PIR.js
@@ -1,3 +1,4 @@
+require('log-timestamp');
 const express = require('express');
 const gpio = require('rpi-gpio');
 const argv = require('minimist')(process.argv.slice(2));
@@ -6,9 +7,13 @@ const port = 45331;
 //get the pin from the command line or default to 7
 const pin = argv["pin"] || 7;
 
+//save the state for when clients connect
+var state = false;
+
 //callback when the state changes
 gpio.on('change', function(channel, value) {
     console.log('Channel ' + channel + ' value is now ' + value);
+    state = value;
     if (value === true) {
         io.sockets.emit('message', { message: 'The table is in use!!!' });
     } else {
@@ -31,5 +36,10 @@ const io = require('socket.io').listen(app.listen(port));
 
 io.on('connection', function (socket) {
     console.log('Client connected');
+    if (state === true) {
+        socket.emit('message', { message: 'The table is in use!!!' });
+    } else {
+        socket.emit('message', { message: 'The table is free!!!' });
+    }
 });
 
